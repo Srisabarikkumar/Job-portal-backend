1df6541da0932b7b1d819de9bb201cc55b265526
srisabarikkumar@gmail.com <srisabarikkumar@gmail.com>
2024-10-02 17:19:17 +0530

    cors url update

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 app.use(cookieParser());
 const corsOptions = {
-    origin:'https://job-portal-backend-nxcr.onrender.com',
+    origin:'https://job-portalapp-mern.netlify.app',
     credentials:true
 }
 
