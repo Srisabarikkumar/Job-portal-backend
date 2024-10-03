82dfe8edb21a072ddcbd791a90fddb71e80ec642
srisabarikkumar@gmail.com <srisabarikkumar@gmail.com>
2024-10-03 17:56:47 +0530

    cookies update

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -89,7 +89,7 @@ export const login = async (req, res) => {
     const tokenData = {
       userId: user._id,
     };
-    const token = jwt.sign(tokenData, process.env.SECRET_KEY, {
+    const token = await jwt.sign(tokenData, process.env.SECRET_KEY, {
       expiresIn: "1d",
     });
 
--- middlewares/isAuthenticated.js
+++ middlewares/isAuthenticated.js
@@ -1,15 +1,18 @@
 import jwt from "jsonwebtoken";
+import dotenv from "dotenv";
+
+dotenv.config({});
 
 const isAuthenticated = async (req, res, next) => {
     try {
-        const token = req.cookies.token;
+        const token = await req.cookies.token;
         if (!token) {
             return res.status(401).json({
                 message: "User not authenticated",
                 success: false,
             })
         }
-        const decode = jwt.verify(token, process.env.SECRET_KEY);
+        const decode = await jwt.verify(token, process.env.SECRET_KEY);
         if(!decode){
             return res.status(401).json({
                 message:"Invalid token",
