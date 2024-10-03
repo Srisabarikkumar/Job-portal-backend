cb532de7fc3ab720b0334c1bdaac4c9e3a60ff76
srisabarikkumar@gmail.com <srisabarikkumar@gmail.com>
2024-10-03 18:46:22 +0530

    new change

--- middlewares/isAuthenticated.js
+++ middlewares/isAuthenticated.js
@@ -5,7 +5,7 @@ dotenv.config({});
 
 const isAuthenticated = async (req, res, next) => {
     try {
-        const token = await req.cookies.token;
+        const token = req.cookies.token;
         if (!token) {
             return res.status(401).json({
                 message: "User not authenticated",
