b1fbfe182d5e66517aa8056f65b882a72a0ec2c1
srisabarikkumar@gmail.com <srisabarikkumar@gmail.com>
2024-10-03 13:15:03 +0530

    new change

--- index.js
+++ index.js
@@ -16,14 +16,27 @@ const app = express();
 
 // middleware
 app.use(express.json());
-app.use(express.urlencoded({extended:true}));
+app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
-const corsOptions = {
-    origin:'https://job-portalapp-mern.netlify.app',
-    credentials:true
-}
 
-app.use(cors(corsOptions));
+const allowedOrigins = [
+  "http://127.0.0.1:8080",
+  "http://localhost:5173",
+  "https://job-portalapp-mern.netlify.app",
+];
+
+app.use(
+  cors({
+    origin: (origin, callback) => {
+      if (allowedOrigins.includes(origin) || !origin) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
+    credentials: true,
+  })
+);
 
 const PORT = process.env.PORT || 3000;
 
@@ -33,6 +46,6 @@ app.use("/api/v1/company", companyRoute);
 app.use("/api/v1/job", jobRoute);
 app.use("/api/v1/application", applicationRoute);
 
-app.listen(PORT,()=>{
-    console.log(`Server running at port ${PORT}`);
-})
+app.listen(PORT, () => {
+  console.log(`Server running at port ${PORT}`);
+});
