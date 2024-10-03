da6a3d92d7cbacc01cc14d28a0ddac4f8f9d590a
srisabarikkumar@gmail.com <srisabarikkumar@gmail.com>
2024-10-03 18:40:36 +0530

    new change

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -107,7 +107,7 @@ export const login = async (req, res) => {
       .cookie("token", token, {
         maxAge: 1 * 24 * 60 * 60 * 1000,
         httpOnly: true,
-        sameSite: "strict",
+        sameSite: "none",
       })
       .json({
         message: `Welcome back ${user.fullname}`,
