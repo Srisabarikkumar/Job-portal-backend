022f44040678c1171a9ca52841fe9c7133deaff1
srisabarikkumar@gmail.com <srisabarikkumar@gmail.com>
2024-10-08 16:55:32 +0530

    new change

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -108,7 +108,7 @@ export const login = async (req, res) => {
     return res
       .status(200)
       .cookie("token", token, {
-        maxAge: 1 * 24 * 60 * 60 * 1000,
+        maxAge: Date.now(),
         httpOnly: true,
         secure: true,
         sameSite: "none",
