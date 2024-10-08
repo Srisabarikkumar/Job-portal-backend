b3a352903422b58e3306ca4e0a998c7db10fc81f
srisabarikkumar@gmail.com <srisabarikkumar@gmail.com>
2024-10-08 18:05:50 +0530

    new change

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -124,8 +124,7 @@ export const login = async (req, res) => {
 };
 export const logout = async (req, res) => {
   try {
-    res.clearCookie("token");
-    return res.status(200).json({
+    return res.status(200).cookie("token", "", { maxAge: 0 }).json({
       message: "Logged out successfully.",
       success: true,
     });
